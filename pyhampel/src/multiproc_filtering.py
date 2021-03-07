de608962b2966bf541b4b645b69181cf9a7b20e0
dwervin <30344880+dwervin@users.noreply.github.com>
2021-03-07 17:40:50 -0600

    test removing main

--- pyhampel/src/multiproc_filtering.py
+++ pyhampel/src/multiproc_filtering.py
@@ -24,31 +24,31 @@ def hampel_mp(g_list: list, vals_col: str, time_col=None, win_size=30, num_dev=3
 
     # import multiprocessing as mp
 
-    global results
+    # global results
     results = []
-    if __name__ == '__main__':
-        # Step 1: Init multiprocessing.Pool()
-        pool = mp.Pool(mp.cpu_count())
+    # if __name__ == '__main__':
+    # Step 1: Init multiprocessing.Pool()
+    pool = mp.Pool(mp.cpu_count())
 
-        # Step 2: Define callback function to collect the output in 'results' to take
-        # advantage of asynchronous calls.
-        def collect_result(result):
-            #global results
-            results.append(result)
+    # Step 2: Define callback function to collect the output in 'results' to take
+    # advantage of asynchronous calls.
+    def collect_result(result):
+        # global results
+        results.append(result)
 
-        # hampel_filter_df(df: pd.DataFrame, vals_col: str, time_col=None, win_size=30, num_dev=3, center_win=True)
+    # hampel_filter_df(df: pd.DataFrame, vals_col: str, time_col=None, win_size=30, num_dev=3, center_win=True)
 
-        # Step 3: Use loop to parallelize. This asynchronous call uses the collect_result
-        # function defined earlier and adds results to results list.
-        # This uses sliding window size of window_size and num_dev standard deviations for outlier detection.
-        for row in g_list:
-            #pool.apply_async(hampel, args=(row, 7, 3), callback=collect_result)
-            #pool.apply_async(hampel, args=(row, window_size, num_dev, True), callback=collect_result)
-            pool.apply_async(hampel_filter_df, args=(row, vals_col, time_col, win_size,
-                                                     num_dev, center_win), callback=collect_result)
+    # Step 3: Use loop to parallelize. This asynchronous call uses the collect_result
+    # function defined earlier and adds results to results list.
+    # This uses sliding window size of window_size and num_dev standard deviations for outlier detection.
+    for row in g_list:
+        #pool.apply_async(hampel, args=(row, 7, 3), callback=collect_result)
+        #pool.apply_async(hampel, args=(row, window_size, num_dev, True), callback=collect_result)
+        pool.apply_async(hampel_filter_df, args=(row, vals_col, time_col, win_size,
+                                                 num_dev, center_win), callback=collect_result)
 
-        # Step 4: Close Pool and let all the processes complete
-        pool.close()
-        pool.join()  # postpones the execution of next line of code until all processes in the queue are done.
+    # Step 4: Close Pool and let all the processes complete
+    pool.close()
+    pool.join()  # postpones the execution of next line of code until all processes in the queue are done.
 
     return results
