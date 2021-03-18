94072e733a339aadd14a07c568ca1fced77103b3
dwervin <30344880+dwervin@users.noreply.github.com>
2021-03-17 23:41:26 -0500

    update setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pyhampel", # Replace with your own username
-    version="0.3.1",
+    version="0.3.2",
     author="Py Hampel",
     author_email="pyhampel@gmail.com",
     description="A flexible package to apply Hampel filter to time series data.  Identify outliers and filter time series.",
