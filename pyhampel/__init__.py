0cb7432f18d5baf0a8b2b0993ac1661da3523617
dwervin <30344880+dwervin@users.noreply.github.com>
2021-03-17 23:54:26 -0500

    update init

--- pyhampel/__init__.py
+++ pyhampel/__init__.py
@@ -3,3 +3,4 @@ from .ingestion.time_series_ingestion import *
 from .utils.ts_utils import *
 from .src.filtering import *
 from .src.multiproc_filtering import *
+from .dataviz.interactive_viz import *
