63e13e6c05fffaa1dbb52a54cb03e9cc9b35fb5c
dwervin <30344880+dwervin@users.noreply.github.com>
2021-03-18 00:55:12 -0500

    add docs

--- pyhampel/src/multiproc_filtering.py
+++ pyhampel/src/multiproc_filtering.py
@@ -7,6 +7,34 @@ __all__ = ["hampel_mp"]
 
 
 def hampel_mp(g_list: list, vals_col: str, time_col=None, win_size=30, num_dev=3, center_win=True):
+    """
+
+    Parameters
+    ----------
+    g_list: list(pd.DataFrame)
+        list of time series dataframes usually generated from gen_list_of_frames() function, but can also be
+        individually compiled list of time series dataframes as long as columns are consistently named in each one.
+    vals_col: str
+        Name of column containing the original values to be processed.
+    time_col: str
+        Name of column containing datetime for time series.  This is optional if dataframe index consists of
+        DatetimeIndex.  If time_col contains string representation of date, it will be converted to datetime64.
+    win_size: int
+        Value of width (time periods) of sliding window.  If time periods are in days, this is the width in number
+        of days.  If time periods are in hours, this is the width in hours.
+    num_dev: int
+        Number of standard deviations used in calculating threshold used to determine an outlier.
+    center_win: boolean
+        Boolean to indicate if point considered is at center of window or leading edge of window.  If False,
+        calculations are similar to moving median with point to be considered on right side of window and points in
+        past used for MAD calculation.
+
+    Returns
+    -------
+    Function returns a list of dataframes,  with each dataframe consisting of original values columns along with
+    the Hampel filtered data, outlier values and boolean flags where outliers found.
+
+    """
 
     # The code for the Hampel filter needed to be added to a separate file for
     # this multiprocessing call to work.  HampelFilter.py needs to be in the local directory.
