2e2ed1643106d0a60d25a7cd64d62a4399f439ad
dwervin <30344880+dwervin@users.noreply.github.com>
2021-03-17 23:24:19 -0500

    update setup

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=["pyhampel","pyhampel.src","pyhampel.utils","pyhampel.ingestion"],
+    packages=["pyhampel","pyhampel.src","pyhampel.utils","pyhampel.ingestion","pyhampel.dataviz"],
     install_requires=[
         'pandas',
         'multiprocessing',
