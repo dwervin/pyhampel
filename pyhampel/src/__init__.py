4f3852c72de68cf428836aa31a85b80e9d6f1c71
dwervin <30344880+dwervin@users.noreply.github.com>
2021-03-21 15:04:01 -0500

    correct reference to module

--- pyhampel/src/__init__.py
+++ pyhampel/src/__init__.py
@@ -10,20 +10,20 @@ from .multiproc_filtering_w_dev import *
 code_hampel_with_dev = 'HampelWithDev.py'
 code_out1 = open(code_hampel_with_dev, 'w')
 
-print("Writing local code file needed for multiprocessing")
+print("Writing local code files needed for multiprocessing")
 
-moda = importlib.import_module('src.HampelWithDev')
+moda = importlib.import_module('pyhampel.src.HampelWithDev')
 src_code = inspect.getsource(moda)
 code_out1.write(src_code)
 code_out1.close()
 
-##########
+# second file
 code_hampel = 'HampelFiltering.py'
 code_out2 = open(code_hampel, 'w')
 
-print("Writing local code file needed for multiprocessing")
+# print("Writing local code file needed for multiprocessing")
 
-modb = importlib.import_module('src.HampelFiltering')
+modb = importlib.import_module('pyhampel.src.HampelFiltering')
 src_code2 = inspect.getsource(modb)
 code_out2.write(src_code2)
 code_out2.close()
