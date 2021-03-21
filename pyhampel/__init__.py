c7a31f8291a37f234a2f0a9f77841bbc5e9c5b6c
dwervin <30344880+dwervin@users.noreply.github.com>
2021-03-21 13:04:58 -0500

    updated .src.init and refactored code

--- pyhampel/__init__.py
+++ pyhampel/__init__.py
@@ -1,6 +1,11 @@
 
 from .ingestion.time_series_ingestion import *
 from .utils.ts_utils import *
+
 from .src.filtering import *
 from .src.multiproc_filtering import *
+
+from .src.filtering_with_dev import *
+from .src.multiproc_filtering_w_dev import *
+
 from .dataviz.interactive_viz import *
